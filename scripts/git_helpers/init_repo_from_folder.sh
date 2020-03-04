b43d1405190ad06a5ee9c12fa8806b16a483e4aa
Jeremy Vagnet <jeremy@curious-company.com>
2020-03-04 16:54:13 +0100

    Enforce using bash for script execution
    Signed-off-by: Jeremy Vagnet <jeremy@curious-company.com>

--- scripts/git_helpers/init_repo_from_folder.sh
+++ scripts/git_helpers/init_repo_from_folder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script initializes an empty git repository (the "target") with a folder from an
 # already existing repo (the "source"), keeping track of the complete git history
