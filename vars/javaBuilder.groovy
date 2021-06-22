abb82c640479af9b35429a61ea36fb3269b457c4
Brice Ruppen <brice.ruppen@armotic.fr>
2021-06-22 12:06:25 +0200

    javaBuilder 2.0.2: Add LFS

--- vars/javaBuilder.groovy
+++ vars/javaBuilder.groovy
@@ -1,6 +1,6 @@
 def call(String version) {
   imagePullPolicy = "IfNotPresent"
-  if (version == '2.0') version = '2.0.1'
+  if (version == '2.0') version = '2.0.2'
   if (version.endsWith('!')) {
     version = version.substring(0, version.length() - 1);
     imagePullPolicy = "Always"
